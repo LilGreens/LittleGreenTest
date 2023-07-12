831d406acfe40280c9ab51154508152984a5e114
LilGreens <dragoncatomorph@gmail.com>
2023-07-12 17:26:43 -0500

    more progress

--- src/main/java/net/littlegreen/testmod/block/ModBlocks.java
+++ src/main/java/net/littlegreen/testmod/block/ModBlocks.java
@@ -1,12 +1,19 @@
 package net.littlegreen.testmod.block;
 
 import net.littlegreen.testmod.TestMod;
+import net.littlegreen.testmod.item.ModCreativeModeTab;
 import net.littlegreen.testmod.item.ModItems;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,15 +25,30 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TestMod.MOD_ID);
 
+    public static final RegistryObject<Block> MISSING_BLOCK = registerBlock("missing_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TEST_TAB);
+
+
+
+    public static final RegistryObject<Block> MISSING_ORE = registerBlock("missing_ore ",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3, 7)), ModCreativeModeTab.TEST_TAB);
+    public static final RegistryObject<Block> DEEPSLATE_MISSING_ORE = registerBlock("deepslate_missing_ore ",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3, 7)), ModCreativeModeTab.TEST_TAB);
+
     public static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-
+        registerBlockItem(name, toReturn, tab);
         return toReturn;
     }
 
-    private static <t extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
                                                                             CreativeModeTab tab) {
-        return ModItems.ITEMS.register(name () -> new Block Item(block.get(), new ItemProperties().tab(tab)));
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
     public static void register(IEventBus eventBus) {
